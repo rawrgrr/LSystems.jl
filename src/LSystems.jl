ef474688fbd51a4c4df0faf0edc1045ab6445b15
Roger Tu <rtu@cal.berkeley.edu>
2015-07-10 21:55:16 -0700

    stream-based approach to a general l-system

--- src/LSystems.jl
+++ src/LSystems.jl
@@ -1,5 +1,24 @@
 module LSystems
 
-# package code goes here
+export lindenmayer
+
+
+function lindenmayer{T<:String, U<:Char, V<:Int}(start::T, transitions::Dict{U, T}, level::V)
+    if level <= 0
+        for c in start
+            produce(c)
+        end
+    else
+        for c in start
+            if haskey(transitions, c)
+                for v in @task lindenmayer(transitions[c], transitions, level - 1)
+                    produce(v)
+                end
+            else
+                produce(c)
+            end
+        end
+    end
+end
 
 end # module
