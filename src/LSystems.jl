bb9f31139fd2a71056bd9cce224d5dbded279d92
Roger Tu <rtu@cal.berkeley.edu>
2015-07-13 23:57:20 -0700

    change from dict to function for handling transitions

--- src/LSystems.jl
+++ src/LSystems.jl
@@ -3,15 +3,27 @@ module LSystems
 export lindenmayer
 
 
+function can_transition{T<:String, U<:Char}(transitions::Dict{U, T}, c::U)
+    return haskey(transitions, c)
+end
+
+function transition{T<:String, U<:Char}(transitions::Dict{U, T}, c::U)
+    return transitions[c]
+end
+
 function lindenmayer{T<:String, U<:Char, V<:Int}(start::T, transitions::Dict{U, T}, level::V)
+    return lindenmayer(start, c -> can_transition(transitions, c), c -> transition(transitions, c), level)
+end
+
+function lindenmayer{T<:String, V<:Int}(start::T, transition_checker::Function, transition_function::Function, level::V)
     if level <= 0
         for c in start
             produce(c)
         end
     else
         for c in start
-            if haskey(transitions, c)
-                for v in @task lindenmayer(transitions[c], transitions, level - 1)
+            if transition_checker(c)
+                for v in @task lindenmayer(transition_function(c), transition_checker, transition_function, level - 1)
                     produce(v)
                 end
             else
